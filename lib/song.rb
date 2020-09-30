c65ab58974eaded3ddcfe814f42d1864ab26da34
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-30 00:56:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,6 +18,13 @@ class Song
       song
   end
   
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+  
 
   def save
     self.class.all << self
