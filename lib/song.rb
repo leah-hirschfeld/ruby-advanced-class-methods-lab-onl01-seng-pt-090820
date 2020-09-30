ec874f63254250d969f5b5206515c33b5444828b
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-30 01:21:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,9 +30,9 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    song = self.find_by_name
+    song = self.find_by_name(name)
     if song == nil 
-      self.create_by_name
+      self.create_by_name(name)
    end
   end
   
