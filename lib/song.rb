15416b49ffdeb0d9e56a77613902a21fe92372ab
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-30 00:20:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,26 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  
+  def initalize
+    end
 
   def self.all
+    @name = name
+    @@all << self
     @@all
   end
+  
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name
+      print self
+  end
+  
 
   def save
     self.class.all << self
